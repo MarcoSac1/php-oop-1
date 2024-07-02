bbd94fcb753c1d28c774184916328cec1bad08a2
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-02 16:16:33 +0200

    created classs and variable of instance

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/Models/db.php';
+
+
+$movie = new Movie();
+$movie -> nomeFilm ='1917';
+$movie -> direttoreFotografia ='Sir Roger Alexander Deakins';
+$movie -> date ='23 luglio 2020';
+
+$movie_2 = new Movie();
+$movie_2 -> nomeFilm ='Paparazzi (film 1998)';
+$movie_2 -> direttoreFotografia ='Carlo Tafani';
+$movie_2 -> date ='18 dicembre 1998';
+
+var_dump($movie, $movie_2);
+?>
